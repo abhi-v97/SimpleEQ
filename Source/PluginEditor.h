cf8113df6f28c55b3372c04c01fa0addd2f9cb3d
abhiv-97 <v.abhishek@outlook.com>
2022-01-17 13:11:41 +0000

    refactored code so custom gui can access peak functions

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -25,7 +25,9 @@ struct CustomRotarySlider : juce::Slider
 //==============================================================================
 /**
 */
-class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor
+class SimpleEQAudioProcessorEditor : public juce::AudioProcessorEditor,
+    juce::AudioProcessorParameter::Listener,
+    juce::Timer
 {
 public:
     SimpleEQAudioProcessorEditor (SimpleEQAudioProcessor&);
@@ -35,10 +37,31 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
+    void parameterValueChanged(int parameterIndex, float newValue) override;
+
+    /** Indicates that a parameter change gesture has started.
+        E.g. if the user is dragging a slider, this would be called with gestureIsStarting
+        being true when they first press the mouse button, and it will be called again with
+        gestureIsStarting being false when they release it.
+        IMPORTANT NOTE: This will be called synchronously, and many audio processors will
+        call it during their audio callback. This means that not only has your handler code
+        got to be completely thread-safe, but it's also got to be VERY fast, and avoid
+        blocking. If you need to handle this event on your message thread, use this callback
+        to trigger an AsyncUpdater or ChangeBroadcaster which you can respond to later on the
+        message thread.
+    */
+    void parameterGestureChanged(int parameterIndex, bool gestureIsStarting) override { }
+
+
+    // query an atomic flag callback to decide if the chain needs updating
+    void timerCallback() override;
+
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor;
+
+    juce::Atomic<bool> parametersChanged{ false };
 
     CustomRotarySlider peakFreqSlider,
     peakGainSlider,
@@ -48,7 +71,21 @@ private:
     lowCutSlopeSlider,
     highCutSlopeSlider;
 
+
+    using APVTS = juce::AudioProcessorValueTreeState;
+    using Attachment = APVTS::SliderAttachment;
+
+    Attachment peakFreqSliderAttachment,
+        peakGainSliderAttachment,
+        peakQualitySliderAttachment,
+        lowCutFreqSliderAttachment,
+        highCutFreqSliderAttachment,
+        lowCutSlopeSliderAttachment,
+        highCutSlopeSliderAttachment;
+
     std::vector<juce::Component*> getComps();
+
+    MonoChain monoChain;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -21,6 +21,7 @@ enum Slope
     Slope_48
 };
 
+
 struct ChainSettings
 {
     float peakFreq{ 0 }, peakGainInDecibels{ 0 }, peakQuality{ 1.f };
@@ -31,8 +32,82 @@ struct ChainSettings
     bool lowCutBypassed{ false }, peakBypassed{ false }, highCutBypassed{ false };
 };
 
+
+//dsp uses a lot of nested namespaces, creating aliases to eliminate namespace and template definitions
+
+using Filter = juce::dsp::IIR::Filter<float>;
+
+using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
+
+// one filter to represent the mono chain
+using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+
+// functions to help clean up the code
+using Coefficients = Filter::CoefficientsPtr;
+void updateCoefficients(Coefficients& old, const Coefficients& replacements);
+Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate);
+
+enum ChainPositions
+{
+    LowCut,
+    Peak,
+    HighCut
+};
+
 // helper function that will return these values
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
+
+template<int Index, typename ChainType, typename CoefficientType>
+void update(ChainType& chain, const CoefficientType& coefficients)
+{
+    updateCoefficients(chain.template get<Index>().coefficients, coefficients[Index]);
+    chain.template setBypassed<Index>(false);
+}
+
+template<typename ChainType, typename CoefficientType>
+void updateCutFilter(ChainType& chain,
+    const CoefficientType& coefficients,
+    const Slope& slope)
+{
+    chain.template setBypassed<0>(true);
+    chain.template setBypassed<1>(true);
+    chain.template setBypassed<2>(true);
+    chain.template setBypassed<3>(true);
+
+    switch (slope)
+    {
+    case Slope_48:
+    {
+        update<3>(chain, coefficients);
+    }
+    case Slope_36:
+    {
+        update<2>(chain, coefficients);
+    }
+    case Slope_24:
+    {
+        update<1>(chain, coefficients);
+    }
+    case Slope_12:
+    {
+        update<0>(chain, coefficients);
+    }
+    }
+}
+
+inline auto makeLowCutFilter(const ChainSettings& chainSettings, double sampleRate)
+{
+    return juce::dsp::FilterDesign<float>::designIIRHighpassHighOrderButterworthMethod(chainSettings.lowCutFreq,
+        sampleRate,
+        2 * (chainSettings.lowCutSlope + 1));
+}
+
+inline auto makeHighCutFilter(const ChainSettings& chainSettings, double sampleRate)
+{
+    return juce::dsp::FilterDesign<float>::designIIRLowpassHighOrderButterworthMethod(chainSettings.highCutFreq,
+        sampleRate,
+        2 * (chainSettings.highCutSlope + 1));
+}
 
 //==============================================================================
 /**
@@ -84,83 +159,11 @@ public:
     juce::AudioProcessorValueTreeState apvts{ *this, nullptr, "Parameters", createParameterLayout() };
 
 private:
-    //dsp uses a lot of nested namespaces, creating aliases to eliminate namespace and template definitions
-     
-    using Filter = juce::dsp::IIR::Filter<float>;
-
-    using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
-
-    // one filter to represent the mono chain
-    using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+    
 
     // two instances of mono for stereo
     MonoChain leftChain, rightChain;
-    
-
-    enum ChainPositions
-    {
-        LowCut, 
-        Peak,
-        HighCut
-    };
-
-    // function to help clean up the code
-    using Coefficients = Filter::CoefficientsPtr;
-    void updateCoefficients(Coefficients& old, const Coefficients& replacements);
-    Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate);
-
-    template<int Index, typename ChainType, typename CoefficientType>
-    void update(ChainType& chain, const CoefficientType& coefficients)
-    {
-        updateCoefficients(chain.template get<Index>().coefficients, coefficients[Index]);
-        chain.template setBypassed<Index>(false);
-    }
-
-    template<typename ChainType, typename CoefficientType>
-    void updateCutFilter(ChainType& chain,
-        const CoefficientType& coefficients,
-        const Slope& slope)
-    {
-        chain.template setBypassed<0>(true);
-        chain.template setBypassed<1>(true);
-        chain.template setBypassed<2>(true);
-        chain.template setBypassed<3>(true);
-
-        switch (slope)
-        {
-        case Slope_48:
-        {
-            update<3>(chain, coefficients);
-        }
-        case Slope_36:
-        {
-            update<2>(chain, coefficients);
-        }
-        case Slope_24:
-        {
-            update<1>(chain, coefficients);
-        }
-        case Slope_12:
-        {
-            update<0>(chain, coefficients);
-        }
-        }
-    }
-
-    inline auto makeLowCutFilter(const ChainSettings& chainSettings, double sampleRate)
-    {
-        return juce::dsp::FilterDesign<float>::designIIRHighpassHighOrderButterworthMethod(chainSettings.lowCutFreq,
-            sampleRate,
-            2 * (chainSettings.lowCutSlope + 1));
-    }
-
-    inline auto makeHighCutFilter(const ChainSettings& chainSettings, double sampleRate)
-    {
-        return juce::dsp::FilterDesign<float>::designIIRLowpassHighOrderButterworthMethod(chainSettings.highCutFreq,
-            sampleRate,
-            2 * (chainSettings.highCutSlope + 1));
-    }
-
+ 
     void updatePeakFilter(const ChainSettings& chainSettings);
     void updateLowCutFilters(const ChainSettings& chainSettings);
     void updateHighCutFilters(const ChainSettings& chainSettings);
